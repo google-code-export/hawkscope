18915a9c4073c50ebd4a471c2ad6678ae1a053fe
tomas.varaneckas <tomas.varaneckas@033809cf-e84d-0410-81fb-211d375de673>
2009-02-26 09:55:21 +0000

    twitter plugin initialization

---
+++ plugins/twitter/trunk/src/main/java/com/varaneckas/hawkscope/plugins/twitter/TwitterPlugin.java
@@ -0,0 +1,22 @@
+package com.varaneckas.hawkscope.plugins.twitter;
+
+import com.varaneckas.hawkscope.plugin.PluginAdapter;
+
+public class TwitterPlugin extends PluginAdapter {
+
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getVersion() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
